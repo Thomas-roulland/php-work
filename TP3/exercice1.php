e4f07ed9c6ea37a84eb902a8e26fd19d5f984011
azkawoila <65232547+azkawoila@users.noreply.github.com>
2020-11-18 09:48:54 +0100

    up

---
+++ TP3/exercice1.php
@@ -0,0 +1,11 @@
+<?php
+
+include "function.php";
+
+?>
+
+
+
+<input type="text" name="nom" id="" placeholder="nom">
+
+<input type="submit" value="envoyer">
---
+++ TP3/function.php
@@ -0,0 +1,36 @@
+<?php
+
+function getHeader($startSession,$title){
+
+    if($startSession){
+        session_start();
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=$title ?></title>
+</head>
+<body>
+    
+
+<?php
+}
+
+function getFooter(){
+    
+?>
+
+    </body>
+</html>
+
+<?php
+
+}
+
+?>
